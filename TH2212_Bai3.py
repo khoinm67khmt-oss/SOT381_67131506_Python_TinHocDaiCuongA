5ab37b3499baef703fe0c4711a47b811f2a0a88e
khoinm67khmt-oss <khoi.nm.67khmt@ntu.edu.vn>
2025-12-22 14:48:47 +0700

    Update TH2212_Bai3.py
    Signed-off-by: khoinm67khmt-oss <khoi.nm.67khmt@ntu.edu.vn>

--- TH2212_Bai3.py
+++ TH2212_Bai3.py
@@ -3,5 +3,8 @@ for i in range(3):
     so = float(input("Nhap so:"))
     ds.append(so)
 lon_nhat = max(ds)
+nho_nhat = min(ds)
 print(f"Danh sach vua nhap la:{ds}")
 print(f"So lon nhat trong danh sach la:{lon_nhat}")
+print(f"So lon nhat trong danh sach la:{nho_nhat}")
+
