dc5168a75528e1c694b599dda4eea7e0d943142d
khoinm67khmt-oss <khoi.nm.67khmt@ntu.edu.vn>
2025-12-22 14:24:26 +0700

    Update TH2212_Bai1.py
    Signed-off-by: khoinm67khmt-oss <khoi.nm.67khmt@ntu.edu.vn>

--- TH2212_Bai1.py
+++ TH2212_Bai1.py
@@ -5,8 +5,11 @@ while True:
         chu_vi = (w + h) * 2
         dien_tich = w * h
         break
+    else:
+        print("Nhap lai")
 print(f"Dien tich HCN la:{dien_tich:.2f}") 
 print(f"Chu vi HCN la:{chu_vi:.2f}")
+
 
 
 
