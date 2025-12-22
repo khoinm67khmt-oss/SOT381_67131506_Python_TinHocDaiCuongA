912ca2026aae3d26eb65164212097bc1efec1c66
khoinm67khmt-oss <khoi.nm.67khmt@ntu.edu.vn>
2025-12-22 14:06:41 +0700

    Add files via upload
    Signed-off-by: khoinm67khmt-oss <khoi.nm.67khmt@ntu.edu.vn>

---
+++ TH2212_Bai1.py
@@ -0,0 +1,10 @@
+while True:
+    w = float(input("Nhap canh w:"))
+    h = float(input("Nhap canh h:"))
+    if (w >= 0.0 and w < 100.0) and (h >= 100.0):
+        chu_vi = (w + h) * 2
+        dien_tich = w * h
+        break
+print(f"Dien tich HCN la:{dien_tich}") 
+print(f"Chu vi HCN la:{chu_vi}")
+
