b7b41881b822d0567ad4f7e052804eaba1a06324
khoinm67khmt-oss <khoi.nm.67khmt@ntu.edu.vn>
2025-12-22 14:45:15 +0700

    Add files via upload
    Signed-off-by: khoinm67khmt-oss <khoi.nm.67khmt@ntu.edu.vn>

---
+++ TH2212_Bai3.py
@@ -0,0 +1,7 @@
+ds =[]
+for i in range(3):
+    so = float(input("Nhap so:"))
+    ds.append(so)
+lon_nhat = max(ds)
+print(f"Danh sach vua nhap la:{ds}")
+print(f"So lon nhat trong danh sach la:{lon_nhat}")
