b547038fd50bc0d37008914505d1f3d8d3a98a48
khoinm67khmt-oss <khoi.nm.67khmt@ntu.edu.vn>
2025-12-22 14:17:13 +0700

    Update TH2212_Bai1.py
    Signed-off-by: khoinm67khmt-oss <khoi.nm.67khmt@ntu.edu.vn>

--- TH2212_Bai1.py
+++ TH2212_Bai1.py
@@ -5,6 +5,7 @@ while True:
         chu_vi = (w + h) * 2
         dien_tich = w * h
         break
-print(f"Dien tich HCN la:{dien_tich}") 
-print(f"Chu vi HCN la:{chu_vi}")
+print(f"Dien tich HCN la:{dien_tich:[.2f]}") 
+print(f"Chu vi HCN la:{chu_vi:[.2f]}")
+
 
